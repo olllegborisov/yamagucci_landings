d5c907361d638cc7b26c71b4658045e2f73d2184
5420 <olllegborisov@gmail.com>
2024-01-11 16:01:13 +0300

    feat: добавил лендинг infinity

--- src/components/pages/PublicProductPage/PublicProductPage.tsx
+++ src/components/pages/PublicProductPage/PublicProductPage.tsx
@@ -12,7 +12,7 @@ const PublicProductPage: FC<PublicProductPageTypes> = ({ data, isLoading }) => (
       isLoading={isLoading}
       {...data?.product}
     />
-    <Landing landingName='miami' />
+    <Landing landingName='infinity' />
   </>
 
 )
