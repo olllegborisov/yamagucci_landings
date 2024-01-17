d8a778ab07543b6188c820e716399bb4995c706c
5420 <olllegborisov@gmail.com>
2024-01-17 18:44:47 +0300

    feat: [YA-landing] - добавил лэндинг apple_way

--- src/components/pages/PublicProductPage/PublicProductPage.tsx
+++ src/components/pages/PublicProductPage/PublicProductPage.tsx
@@ -12,7 +12,7 @@ const PublicProductPage: FC<PublicProductPageTypes> = ({ data, isLoading }) => (
       isLoading={isLoading}
       {...data?.product}
     />
-    <Landing landingName='miami' />
+    <Landing landingName='{data?.product?.system_name}' />
   </>
 )
 
