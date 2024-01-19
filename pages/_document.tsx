6cac90730e53aafe1e8e38292a0f77da550e82d1
Vladimir <105880918+tacticSugar@users.noreply.github.com>
2024-01-19 13:49:23 +0300

    feat: favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,40 @@ import { FC } from 'react'
 const Document: FC = () => (
   <Html lang='ru'>
     <Head>
+      {/* favicon */}
+      <link
+        href='/favicon/favicon.ico'
+        rel='shortcut icon'
+        type='image/x-icon'
+      />
+      <link
+        href='/favicon/android-chrome-192x192.png'
+        rel='icon'
+        sizes='192x192'
+        type='image/png'
+      />
+      <link
+        href='/favicon/apple-touch-icon.png'
+        rel='apple-touch-icon'
+        sizes='180x180'
+      />
+      <link
+        href='/favicon/favicon-32x32.png'
+        rel='icon'
+        sizes='32x32'
+        type='image/png'
+      />
+      <link
+        href='/favicon/favicon-16x16.png'
+        rel='icon'
+        sizes='16x16'
+        type='image/png'
+      />
+      <link
+        href='/favicon/site.webmanifest'
+        rel='manifest'
+      />
+      {/* noindex */}
       <meta
         content='nositelinkssearchbox'
         key='sitelinks'
@@ -25,6 +59,18 @@ const Document: FC = () => (
       />
     </Head>
     <body >
+      <noscript>
+        <div style={{ textAlign: 'center' }}>
+          Для полной функциональности этого сайта необходимо включить JavaScript.
+          <a
+            href='https://www.enable-javascript.com/ru/'
+            style={{ color: 'blue', display: 'block', textDecoration: 'underline' }}
+          >
+            Вот инструкции, как включить JavaScript в вашем браузере
+          </a>
+          .
+        </div>
+      </noscript>
       <Main />
       <NextScript />
     </body>
